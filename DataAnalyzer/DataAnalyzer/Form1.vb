540fdbafcaa89c16b6e75b2edabfa079b869cce8
Kasai Go (Home Desktop) <kg0425@asagi.waseda.jp>
2020-12-13 13:30:55 +0900

    Pull test

--- DataAnalyzer/DataAnalyzer/Form1.vb
+++ DataAnalyzer/DataAnalyzer/Form1.vb
@@ -28,6 +28,10 @@ Public Class Form1
     Dim Signal_Strength() As MyData2D
     Dim signal11, signal12, signal13, signal14, signal15, signal16 As MyData2D
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         Dim PathOP As String
